3a3c22e59985369c70b225edda37f4483bd844f9
JesonNehru <jesonsaral56@gmail.com>
2025-06-05 10:45:05 -0400

    Added /metrics endpoint using prom-client

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const express = require('express');
+const client = require('prom-client');
+
 const app = express();
-const PORT = 3000;
+const register = new client.Registry();
+
+// Collect default metrics
+client.collectDefaultMetrics({ register });
+
 app.get('/', (req, res) => {
-  res.send('Hello from App1!');
+  res.send('Hello from app1!');
 });
 
-app.listen(PORT, () => {
-  console.log(`🚀 Server is running at http://localhost:${PORT}`);
+// Metrics endpoint
+app.get('/metrics', async (req, res) => {
+  res.set('Content-Type', register.contentType);
+  res.end(await register.metrics());
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
 });
